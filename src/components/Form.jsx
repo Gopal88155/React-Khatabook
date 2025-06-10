34c26f64d2c7552c06717e7ce2af18bb453d1c4e
Gopal88155 <gopalthakur88155@gmail.com>
2025-06-10 14:28:50 +0530

    form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,53 +1,59 @@
-import React, { useContext, useEffect, useState } from 'react'
-import TransactionContext from '../context/TransactionContext'
+import React, { useContext, useEffect, useState } from "react";
+import TransactionContext from "../context/TransactionContext";
 
 const Form = () => {
+  const { dispatch, edit } = useContext(TransactionContext);
 
-    const { dispatch, edit } = useContext(TransactionContext)
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const [text, setText] = useState("")
-    const [amount, setAmount] = useState("")
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        !edit.isEdit ? dispatch({
-            type: "ADD_TRANSACTION",
-            payload: { id: crypto.randomUUID(), text, amount: +amount }
-        }) : dispatch({
-            type: "UPDATE_TRANSACTION",
-            payload: { id: edit.transaction.id, text, amount: +amount }
+    !edit.isEdit
+      ? dispatch({
+          type: "ADD_TRANSACTION",
+          payload: { id: crypto.randomUUID(), text, amount: +amount },
         })
+      : dispatch({
+          type: "UPDATE_TRANSACTION",
+          payload: { id: edit.transaction.id, text, amount: +amount },
+        });
 
-        setText("")
-        setAmount("")
-    }
+    setText("");
+    setAmount("");
+  };
 
-    useEffect(() => {
-        setText(edit.transaction.text)
-        setAmount(edit.transaction.amount)
-    }, [edit])
+  useEffect(() => {
+    setText(edit.transaction?.text || "");
+    setAmount(edit.transaction?.amount?.toString() || "");
+  }, [edit]);
 
-    return (
-        <form className="my-3" onSubmit={(e) => handleSubmit(e)}>
-            <input type="text"
-                className="form-control rounded-0 my-2"
-                placeholder="Enter Transaction"
-                value={text}
-                onChange={e => setText(e.target.value)}
-                required />
+  return (
+    <form className="my-3" onSubmit={(e) => handleSubmit(e)}>
+      <input
+        type="text"
+        className="form-control rounded-0 my-2"
+        placeholder="Enter Transaction"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        required
+      />
 
-            <input type="number"
-                className="form-control rounded-0 my-2"
-                placeholder="Enter Amount"
-                value={amount}
-                onChange={e => setAmount(e.target.value)}
-                required />
+      <input
+        type="number"
+        className="form-control rounded-0 my-2"
+        placeholder="Enter Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        required
+      />
 
-            <button className="btn btn-success w-100 rounded-0 mt-3" >Save Transaction</button>
-        </form>
-    )
-}
+      <button className="btn btn-success w-100 rounded-0 mt-3">
+        Save Transaction
+      </button>
+    </form>
+  );
+};
 
-export default Form
+export default Form;
